4b0374db74579afb0dc9ded3ab4bffceb10e05c5
Azmara Womble <xazmara@yahoo.com>
2024-08-16 12:28:32 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+use strict';
+function logger(){
+    console.log('My name is Azmara');
+}
+
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges){
+    console.log(apples,oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice
+}
+
+fruitProcessor(5,0);
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
